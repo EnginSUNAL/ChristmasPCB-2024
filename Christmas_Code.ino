1f89164abde2993bf498f8c5020fdc2f9a3414c1
EnginSUNAL <enginsunal@gmail.com>
2025-06-29 15:06:20 +0300

    Add files via upload

---
+++ Christmas_Code.ino
@@ -0,0 +1,123 @@
+#define LED_PIN_1 0
+#define LED_PIN_2 1
+#define LED_PIN_3 2
+#define LED_PIN_4 3
+#define LED_PIN_5 4
+
+
+void setup() {
+  pinMode(LED_PIN_1, OUTPUT);
+  pinMode(LED_PIN_2, OUTPUT);
+  pinMode(LED_PIN_3, OUTPUT);
+  pinMode(LED_PIN_4, OUTPUT);
+  pinMode(LED_PIN_5, OUTPUT);
+}
+
+void loop() {
+  
+  digitalWrite(LED_PIN_1, HIGH);
+  digitalWrite(LED_PIN_2, LOW);
+  digitalWrite(LED_PIN_3, LOW);
+  digitalWrite(LED_PIN_4, LOW);
+  digitalWrite(LED_PIN_5, LOW);  
+  delay(1000);
+  
+  digitalWrite(LED_PIN_1, LOW);
+  digitalWrite(LED_PIN_2, HIGH);
+  digitalWrite(LED_PIN_3, LOW);
+  digitalWrite(LED_PIN_4, LOW);
+  digitalWrite(LED_PIN_5, LOW);  
+  delay(1000);
+  
+  digitalWrite(LED_PIN_1, LOW);
+  digitalWrite(LED_PIN_2, LOW);
+  digitalWrite(LED_PIN_3, HIGH);
+  digitalWrite(LED_PIN_4, LOW);
+  digitalWrite(LED_PIN_5, LOW);  
+  delay(1000);
+  
+  digitalWrite(LED_PIN_1, LOW);
+  digitalWrite(LED_PIN_2, LOW);
+  digitalWrite(LED_PIN_3, LOW);
+  digitalWrite(LED_PIN_4, HIGH);
+  digitalWrite(LED_PIN_5, LOW);  
+  delay(1000);
+  
+  digitalWrite(LED_PIN_1, LOW);
+  digitalWrite(LED_PIN_2, LOW);
+  digitalWrite(LED_PIN_3, LOW);
+  digitalWrite(LED_PIN_4, LOW);
+  digitalWrite(LED_PIN_5, HIGH);  
+  delay(1000);
+  
+  digitalWrite(LED_PIN_1, HIGH);
+  digitalWrite(LED_PIN_2, LOW);
+  digitalWrite(LED_PIN_3, LOW);
+  digitalWrite(LED_PIN_4, LOW);
+  digitalWrite(LED_PIN_5, LOW);  
+  delay(1000);
+    
+  digitalWrite(LED_PIN_1, HIGH);
+  digitalWrite(LED_PIN_2, HIGH);
+  digitalWrite(LED_PIN_3, LOW);
+  digitalWrite(LED_PIN_4, LOW);
+  digitalWrite(LED_PIN_5, LOW);  
+  delay(1000);
+    
+  digitalWrite(LED_PIN_1, HIGH);
+  digitalWrite(LED_PIN_2, HIGH);
+  digitalWrite(LED_PIN_3, HIGH);
+  digitalWrite(LED_PIN_4, LOW);
+  digitalWrite(LED_PIN_5, LOW);  
+  delay(1000);
+    
+  digitalWrite(LED_PIN_1, HIGH);
+  digitalWrite(LED_PIN_2, HIGH);
+  digitalWrite(LED_PIN_3, HIGH);
+  digitalWrite(LED_PIN_4, HIGH);
+  digitalWrite(LED_PIN_5, LOW);  
+  delay(1000);
+    
+  digitalWrite(LED_PIN_1, HIGH);
+  digitalWrite(LED_PIN_2, HIGH);
+  digitalWrite(LED_PIN_3, HIGH);
+  digitalWrite(LED_PIN_4, HIGH);
+  digitalWrite(LED_PIN_5, HIGH);  
+  delay(1000);
+    
+  digitalWrite(LED_PIN_1, LOW);
+  digitalWrite(LED_PIN_2, LOW);
+  digitalWrite(LED_PIN_3, LOW);
+  digitalWrite(LED_PIN_4, LOW);
+  digitalWrite(LED_PIN_5, LOW);  
+  delay(1000);  
+    
+  digitalWrite(LED_PIN_1, HIGH);
+  digitalWrite(LED_PIN_2, HIGH);
+  digitalWrite(LED_PIN_3, HIGH);
+  digitalWrite(LED_PIN_4, HIGH);
+  digitalWrite(LED_PIN_5, HIGH);  
+  delay(1000);
+    
+  digitalWrite(LED_PIN_1, LOW);
+  digitalWrite(LED_PIN_2, LOW);
+  digitalWrite(LED_PIN_3, LOW);
+  digitalWrite(LED_PIN_4, LOW);
+  digitalWrite(LED_PIN_5, LOW);  
+  delay(1000);
+      
+  digitalWrite(LED_PIN_1, HIGH);
+  digitalWrite(LED_PIN_2, HIGH);
+  digitalWrite(LED_PIN_3, HIGH);
+  digitalWrite(LED_PIN_4, HIGH);
+  digitalWrite(LED_PIN_5, HIGH);  
+  delay(1000);
+    
+  digitalWrite(LED_PIN_1, LOW);
+  digitalWrite(LED_PIN_2, LOW);
+  digitalWrite(LED_PIN_3, LOW);
+  digitalWrite(LED_PIN_4, LOW);
+  digitalWrite(LED_PIN_5, LOW);  
+  delay(1000);
+    
+}
